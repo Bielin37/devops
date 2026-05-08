39941e0043e157c9225ee778daa4615ba2a44a0e
Bielin37 <41125221+Bielin37@users.noreply.github.com>
2026-05-08 11:41:36 +0200

    update github actions

--- devops/terraform/main.tf
+++ devops/terraform/main.tf
@@ -100,6 +100,11 @@ resource "aws_instance" "app" {
 
   key_name = "redwood-key"
 
+  root_block_device {
+    volume_size = 20
+    volume_type = "gp3"
+  }
+
   tags = {
     Name = "redwood-app"
   }
